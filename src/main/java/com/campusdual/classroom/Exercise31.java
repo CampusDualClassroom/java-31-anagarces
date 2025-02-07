f27d05eaf38d4544b5841310b5a433bee82df56c
Ana Garces <anagarcestech@gmail.com>
2025-02-07 14:59:40 +0100

    Solucion ejercicio 31

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,48 @@
 package com.campusdual.classroom;
 
+import java.io.*;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
 
+        fileReader(new File("src/main/resources/lorem.txt"));
+
     }
+
+
+    //metodo estatico para leer ficheros
+    public static void fileReader(File fileSource) {
+        FileReader fr = null;
+        BufferedReader br = null;
+        try{
+            fr = new FileReader(fileSource);
+             br = new BufferedReader(fr);
+            String linea = "";
+
+            while((linea = br.readLine()) != null){
+                System.out.println(linea);
+            }
+        } catch (FileNotFoundException ex){
+            System.out.println("Fichero no encontrado. ");
+            ex.printStackTrace();
+        } catch (IOException ex2){
+            System.out.println("Fallo al leer lineas del fichero");
+            ex2.printStackTrace();
+        } finally {
+            try{
+                if(fr != null || br != null){
+                    fr.close();
+                    br.close();
+                }
+            } catch (IOException ex3){
+                System.out.println("No se ha podido cerrar algun flujo");
+                ex3.printStackTrace();
+            }
+        }
+
+
+
+    }
+
 }
